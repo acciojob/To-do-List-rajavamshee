f929189f900f329ba4521f115e543b9ed8ca5885
Rajavamshee_123 <69705134+rajavamshee@users.noreply.github.com>
2023-02-08 22:38:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
 
+function myFunc() {
+	let insertedValue = document.getElementById("newTodoInput").value;
+	if(insertedValue != "") {
+		let orderedList = document.getElementsByTagName("ol")[0];
+		let createList = document.createElement("li");
+		createList.textContent = insertedValue;
+		orderedList.appendChild(createList);
+		document.getElementById("newTodoInput").value = "";
+	}
+}
